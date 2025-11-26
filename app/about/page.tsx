b952195f1bcfaba739f3b55e6eb4f6ab1227fc6a
cfranklin-uturn <cfranklin@uturndata.com>
2025-11-26 02:48:34 -0800

    Lots of changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+import { MainArea } from "@/components/main-area"
 import { AppSidebar } from "@/components/app-sidebar"
 import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
@@ -22,8 +23,8 @@ export default function About() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
-              <div className="px-4 lg:px-6">
+              <div className="px-4 lg:px-6"> 
+              <MainArea />
               </div>
             </div>
           </div>
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -4,13 +4,7 @@ import * as React from "react"
 import Image from 'next/image'
 import Link from 'next/link'
 import {
-  IconCamera,
-  IconFileAi,
-  IconFileDescription,
   IconFolderFilled,
-  IconHelp,
-  IconSearch,
-  IconSettings,
   IconHomeFilled,
   IconUserFilled,
   IconBriefcaseFilled,
@@ -21,8 +15,6 @@ import {
 
 import { NavDocuments } from "@/components/nav-documents"
 import { NavMain } from "@/components/nav-main"
-import { NavSecondary } from "@/components/nav-secondary"
-import { NavUser } from "@/components/nav-user"
 import {
   Sidebar,
   SidebarContent,
@@ -66,71 +58,6 @@ const data = {
       icon: IconArticleFilled,
     },
   ],
-  navClouds: [
-    {
-      title: "Capture",
-      icon: IconCamera,
-      isActive: true,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Proposal",
-      icon: IconFileDescription,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Prompts",
-      icon: IconFileAi,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  navSecondary: [
-    {
-      title: "Settings",
-      url: "#",
-      icon: IconSettings,
-    },
-    {
-      title: "Get Help",
-      url: "#",
-      icon: IconHelp,
-    },
-    {
-      title: "Search",
-      url: "#",
-      icon: IconSearch,
-    },
-  ],
   documents: [
     {
       name: "Music Production",
@@ -171,10 +98,8 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarContent>
         <NavMain items={data.navMain} />
         <NavDocuments items={data.documents} />
-        <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
       </SidebarFooter>
     </Sidebar>
   )
